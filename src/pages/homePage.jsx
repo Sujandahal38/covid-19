85b06d5914ed38e76fdac0e407ab754cb2655e53
Sujan Dahal <sujandahal38@gmail.com>
2020-09-08 00:28:14 +0545

    podcast removed :(

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import NepaliCardInfo from '../components/cards/NepalInfoCard';
 import News from '../components/cards/News';
-import PodcastSection from './PodcastSection';
 
 export default function HomePage(props) {
   return(
    <>
     <NepaliCardInfo/>
-    <PodcastSection/>
     <News newsLength={10}/>
    </>
   )
